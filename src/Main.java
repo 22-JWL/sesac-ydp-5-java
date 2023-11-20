d74f2bccef658da3d1563b368404961b71ca1289
22-JWL <jck1004ee@gmail.com>
2023-11-20 15:47:16 +0900

    study: print, main class, main mathod

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+//메인 클래스 (시작 클래스) 정의
+// - main 메서드 가짐
+// - 메인 클래스는 한 개!(node에서 진입점 파일 역할)
+public class Main {//SesacStudent
+
+    //main 메소드
+    // - 자바 응용 프토그램의 실행 시작점
+    // - 애플리케이션이 실행되려면 최소 1개 존재해야 함
+    // - JVM 은 세미콜론으로 끝나는 문장을 하나의 명령문으로 인식
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+
+        System.out.print("Java Study Start!" + "haha");
+        System.out.println("Yeah");
         System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+
+
         for (int i = 1; i <= 10; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
     }
